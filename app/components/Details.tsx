d2029edccf5944252e5ad1fc2b0a6abcaa3d24b0
Zulfit <106056151+Zulfit@users.noreply.github.com>
2026-04-17 17:51:30 +0800

    push details

---
+++ app/components/Details.tsx
@@ -0,0 +1,13 @@
+export default function Details() {
+    return (
+        <section className="relative min-h-dvh w-full max-w-md mx-auto flex flex-col items-center py-16 px-6 overflow-hidden bg-[#F8F4EF] sm:border-x sm:border-gray-200">
+            {/* Header */}
+            <div className="w-full mb-20 relative">
+                <h2 className="text-[34px] sm:text-4xl font-serif text-[#4a453e] mb-1 z-10 relative">Program of the day</h2>
+                <h3 className="text-3xl sm:text-[34px] font-script text-[#b5a999] absolute -bottom-5 right-4 z-0">
+                    wedding schedule
+                </h3>
+            </div>
+        </section>
+    );
+}
