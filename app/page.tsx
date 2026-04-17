2158d43ad7a6eab0795d2033e68908675d62e2fe
Zulfit <106056151+Zulfit@users.noreply.github.com>
2026-04-17 17:49:27 +0800

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,31 @@
 import Hero from "./components/Hero";
+import Countdown from "./components/Countdown";
+import Tentative from "./components/Tentative";
+import Details from "./components/Details";
+import ReactPlayer from "react-player";
 
 export default function Home() {
   return (
-    <Hero 
-      bride="Halima"
-      groom="Yunus" 
-      date={{
-        month: "DECEMBER",
-        dayName: "SUNDAY",
-        dayNumber: "30",
-        year: "2026",
-        time: "AT 5 PM"
-      }}
-      location={["43 MEAM STREET", "BLACKBURN", "BB19TQ"]} 
-    />
+    <>
+      <ReactPlayer
+        src="https://youtu.be/kH7wlLOQMNM?si=-hTop1Ghs793-TPV"
+        playing={true}
+        style={{ display: "none" }}
+      />
+      <Hero
+        bride="Halima"
+        groom="Yunus"
+        date={{
+          month: "DECEMBER",
+          dayName: "SUNDAY",
+          dayNumber: "30",
+          year: "2026",
+          time: "AT 5 PM"
+        }}
+        location={["43 MEAM STREET", "BLACKBURN", "BB19TQ"]} />
+      <Details />
+      <Countdown targetDate={new Date("2026-12-30T17:00:00")} />
+      <Tentative />
+    </>  
   );
 }
